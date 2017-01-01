f5904eb02b0723b944d91814f09fd3a43c34083b
Itay Perl <itay.perl@gmail.com>
2017-01-01 05:01:03 +0200

    partial fix for double-click

--- content.js
+++ content.js
@@ -1,9 +1,17 @@
 $(function() {
+    var busy = false;
     $('#primary .concept_light-wrapper').each(function() {
         var word = $(this).find('.concept_light-readings .text').text().trim();
         var href = $('<a class="concept_light-status_link">Play on Forvo</a>').insertBefore($(this).parent().find('.concept_light-status > a').first());
         href.click(function() {
+            if (busy)
+            {
+                return;
+            }
+
+            busy = true;
             chrome.runtime.sendMessage(null, {word: word}, function(obj) {
+                busy = false;
                 if (obj !== null) {
                     $(obj)[0].play();
                 }
